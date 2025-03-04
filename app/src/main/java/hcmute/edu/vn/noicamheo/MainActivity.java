10f27efbeb904143714807aa2ff81507f11985bd
“myuh250” <huyng2724@gmail.com>
2025-03-05 03:00:39 +0700

    First verion of main layout

--- app/src/main/java/hcmute/edu/vn/noicamheo/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/noicamheo/MainActivity.java
@@ -1,6 +1,9 @@
 package hcmute.edu.vn.noicamheo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fabMain, btnOption1, btnOption2;
+    private boolean isExpanded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,32 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        MaterialToolbar toolbar = findViewById(R.id.tb_home);
+        setSupportActionBar(toolbar);
+
+        fabMain = findViewById(R.id.fab_main);
+        btnOption1 = findViewById(R.id.btn_option1);
+        btnOption2 = findViewById(R.id.btn_option2);
+
+        // Load animation
+        Animation openAnim = AnimationUtils.loadAnimation(this, R.anim.fab_open);
+        Animation closeAnim = AnimationUtils.loadAnimation(this, R.anim.fab_close);
+
+        fabMain.setOnClickListener(v -> {
+            if (isExpanded) {
+                btnOption1.startAnimation(closeAnim);
+                btnOption2.startAnimation(closeAnim);
+                btnOption1.setVisibility(View.GONE);
+                btnOption2.setVisibility(View.GONE);
+            } else {
+                btnOption1.startAnimation(openAnim);
+                btnOption2.startAnimation(openAnim);
+                btnOption1.setVisibility(View.VISIBLE);
+                btnOption2.setVisibility(View.VISIBLE);
+            }
+            isExpanded = !isExpanded;
         });
     }
 }
