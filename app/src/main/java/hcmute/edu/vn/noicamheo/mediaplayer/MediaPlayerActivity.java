5cd451eca6f222f28b0ba9899730ee5378c739bc
“myuh250” <huyng2724@gmail.com>
2025-03-07 01:59:20 +0700

    MediaPlayer class

--- app/src/main/java/hcmute/edu/vn/noicamheo/mediaplayer/MediaPlayerActivity.java
+++ app/src/main/java/hcmute/edu/vn/noicamheo/mediaplayer/MediaPlayerActivity.java
@@ -1,4 +1,38 @@
 package hcmute.edu.vn.noicamheo.mediaplayer;
 
-public class MediaPlayerActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.SearchView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import hcmute.edu.vn.noicamheo.MainActivity;
+import hcmute.edu.vn.noicamheo.R;
+
+public class MediaPlayerActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_mediaplay);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mediaplay), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        androidx.appcompat.widget.SearchView searchView = findViewById(R.id.sv_media);
+        searchView.setQuery("Music", false);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        startActivity(intent);
+    }
 }
