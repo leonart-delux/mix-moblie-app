acb6953077c40bc32b4c1fe5a48eb0466e684834
“myuh250” <huyng2724@gmail.com>
2025-03-07 13:04:54 +0700

    Refactor file name activity_main to activity_menu

--- app/src/main/java/hcmute/edu/vn/noicamheo/MenuActivity.java
+++ app/src/main/java/hcmute/edu/vn/noicamheo/MenuActivity.java
@@ -26,7 +26,7 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
